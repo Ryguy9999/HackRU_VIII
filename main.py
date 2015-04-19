96238fd798f335ce247ce6ae9f2b00da37e14652
Jason C <jasoncarrete5@gmail.com>
2015-04-18 21:25:30 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,30 @@
+'''
+
+@author: Jason Carrete
+'''
+from flask import Flask, request, redirect
+import twilio.twiml, threading, pygame, sys
+from pygame.locals import *
+
+def game():
+    pygame.init()
+    display = pygame.display.set_mode((640, 480), 0, 32)
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+                pygame.quit()
+gameThread = threading.Thread(target = game)
+gameThread.start()
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_monkey():
+    """Respond to incoming calls with a simple text message."""
+
+    resp = twilio.twiml.Response()
+    resp.message("Hello, Mobile Monkey")
+    return str(resp)
+if __name__ == "__main__":
+    app.run(debug=True)
