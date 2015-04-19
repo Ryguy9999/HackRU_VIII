76ea988cc608f74ec301312650fa4aad624ac3ee
Jason C <jasoncarrete5@gmail.com>
2015-04-18 23:27:47 -0400

    receives messages

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ app = Flask(__name__)
 account_sid = "PNdfd2b6f558dc8ff0e72877df59a1a63b"
 auth_token = "fe17ee3c605d7e44cb2462556f986b99"
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route('/', methods=["GET", "POST"])
 def hello_monkey(): #respond to text
-    body = request.form['Body']
+    body = request.form["Body"]
     print("{}".format(body))
     return ""
 
